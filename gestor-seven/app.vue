9ccec8ec4c7d33ee35662d68c37c772a547fe201
Adriano Ruiz Boldarini <ruiz@7cliques.com.br>
2025-03-11 22:15:48 -0300

    ok

---
+++ gestor-seven/app.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <MedicalDashboard />
+  </div>
+</template>
+
+<script setup>
+import MedicalDashboard from '~/components/MedicalDashboard.vue';
+</script>
--- site-land-page-venda/components/secao_planos.vue
+++ site-land-page-venda/components/secao_planos.vue
@@ -25,48 +25,48 @@
 </template>
 
 <script setup lang="ts">
-import CardPlano from '~/components/card_plano.vue'
+import CardPlano from "~/components/card_plano.vue";
 
 const planos = [
   {
-    nome: 'Essencial',
-    preco: 'R$ 149',
+    nome: "Essencial",
+    preco: "R$ 150",
     destaque: false,
     recursos: [
-      'Agenda Médica Inteligente',
-      'Agendamento Online',
-      'Cadastro de Pacientes',
-      'Prontuário Eletrônico Básico',
-      'Prescrição Digital',
-      'Lembretes automáticos por WhatsApp'
-    ]
+      "Agenda Médica Inteligente",
+      "Agendamento Online",
+      "Cadastro de Pacientes",
+      "Prontuário Eletrônico Básico",
+      "Prescrição Digital",
+      "Lembretes automáticos por WhatsApp",
+    ],
   },
   {
-    nome: 'Clínico',
-    preco: 'R$ 199',
+    nome: "Clínico",
+    preco: "R$ 300",
     destaque: true,
     recursos: [
-      'Todos os recursos do Essencial',
-      'Assinatura Digital Avançada',
-      'Confirmação Inteligente de Consultas via WhatsApp',
-      'Geração assistida de prescrições médicas',
-      'Sugestões de texto para prontuários',
-      'Automatização de pós-consulta via WhatsApp'
-    ]
+      "Todos os recursos do Essencial",
+      "Assinatura Digital Avançada",
+      "Confirmação Inteligente de Consultas via WhatsApp",
+      "Geração assistida de prescrições médicas",
+      "Sugestões de texto para prontuários",
+      "Automatização de pós-consulta via WhatsApp",
+    ],
   },
   {
-    nome: 'Especialista',
-    preco: 'R$ 299',
+    nome: "Especialista",
+    preco: "R$ 500",
     destaque: false,
     recursos: [
-      'Todos os recursos do Clínico',
-      'Prontuário estruturado por especialidade',
-      'Reconhecimento de voz para ditado de prontuários',
-      'Assistente virtual para gestão da clínica',
-      'Busca avançada no histórico de pacientes',
-      'Relatórios clínicos e administrativos',
-      'Certificação LGPD e segurança avançada'
-    ]
-  }
-]
+      "Todos os recursos do Clínico",
+      "Prontuário estruturado por especialidade",
+      "Reconhecimento de voz para ditado de prontuários",
+      "Assistente virtual para gestão da clínica",
+      "Busca avançada no histórico de pacientes",
+      "Relatórios clínicos e administrativos",
+      "Certificação LGPD e segurança avançada",
+    ],
+  },
+];
 </script>
