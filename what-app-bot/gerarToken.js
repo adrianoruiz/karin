ec7e9e7009d6ee9eda41d8f3d7e7818762054526
Adriano Ruiz Boldarini <ruiz@7cliques.com.br>
2026-03-07 12:38:10 -0300

    ok

--- what-app-bot/gerarToken.js
+++
@@ -1,5 +0,0 @@
-// gerarToken.js
-const authMiddleware = require('./src/middleware/auth');
-
-const token = authMiddleware.getToken();
-console.log('Token de autenticação:', token);
--- what-app-bot/src/services/logger.js
+++
@@ -1,9 +0,0 @@
-// src/services/logger.js
-const logger = {
-  debug: (...args) => console.debug('[DEBUG]', ...args),
-  info: (...args) => console.info('[INFO]', ...args),
-  warn: (...args) => console.warn('[WARN]', ...args),
-  error: (...args) => console.error('[ERROR]', ...args),
-};
-
-module.exports = logger; 
